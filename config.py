72a1a00f27ff7bd24b290630938b71997d534c3f
leaingmuch <2476511863@qq.com>
2023-05-09 12:49:26 +0800

    Add new files

---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+from pathlib import Path
+#
+# show_file = Path('./show')
+show_file = Path('./show_da')
---
+++ order.py
@@ -0,0 +1,2 @@
+# python main.py --resume ./weights/detr_resnet50.pth
+# 验证 --eval
